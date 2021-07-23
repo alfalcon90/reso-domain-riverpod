96ce88b6277abed12ecd39d6d9418dbe73597630
Alberto Vildosola <56562400+alfalcon90@users.noreply.github.com>
2021-07-23 15:46:54 -0700

    move root router to app.dart

--- lib/view/core/app.dart
+++ lib/view/core/app.dart
@@ -1,14 +1,30 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:chat/state/auth/auth_providers.dart';
+import 'package:chat/state/core/app_launch_providers.dart';
 import 'package:flutter/material.dart';
 import 'package:chat/view/routes/app_router.gr.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class App extends StatelessWidget {
+class App extends ConsumerWidget {
   final _appRouter = AppRouter();
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final launchState = ref.watch(appLaunchProvider);
+    final authState = ref.watch(authProvider);
+
     return MaterialApp.router(
       title: 'Notes',
-      routerDelegate: _appRouter.delegate(),
+      routerDelegate: AutoRouterDelegate.declarative(
+        _appRouter,
+        routes: (_) => [
+          launchState.maybeMap(
+              orElse: () => SplashRoute(),
+              data: (_) => authState.maybeMap(
+                  orElse: () => SignInRoute(),
+                  authenticated: (_) => NotesRouter()))
+        ],
+      ),
       routeInformationParser: _appRouter.defaultRouteParser(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light().copyWith(
--- lib/view/routes/app_router.dart
+++ lib/view/routes/app_router.dart
@@ -4,31 +4,25 @@ import 'package:chat/view/core/splash_page.dart';
 import 'package:chat/view/notes/note_form/note_form_page.dart';
 import 'package:chat/view/notes/notes_overview/notes_overview_page.dart';
 import 'package:chat/view/routes/notes_router.dart';
-import 'package:chat/view/routes/root_router.dart';
 
-@AdaptiveAutoRouter(
-  replaceInRouteName: 'Page,Route',
-  routes: <AutoRoute>[
-    AutoRoute(path: '/', page: RootRouter, initial: true, children: [
-      // Splash Page
-      AutoRoute(
-        path: '',
-        page: SplashPage,
-      ),
+@AdaptiveAutoRouter(replaceInRouteName: 'Page,Route', routes: <AutoRoute>[
+  // Splash Page
+  AutoRoute(
+    path: '/',
+    page: SplashPage,
+  ),
 
-      // Note Routes
-      AutoRoute(path: '', page: NotesRouter, children: [
-        AutoRoute(path: '', page: NotesOverviewPage),
-        AutoRoute(
-          path: 'note/*',
-          page: NoteFormPage,
-          fullscreenDialog: true,
-        ),
-      ]),
+  // Note Routes
+  AutoRoute(path: '', page: NotesRouter, children: [
+    AutoRoute(path: '', page: NotesOverviewPage),
+    AutoRoute(
+      path: 'note/*',
+      page: NoteFormPage,
+      fullscreenDialog: true,
+    ),
+  ]),
 
-      // Sign In Route
-      AutoRoute(path: 'signin/', page: SignInPage),
-    ]),
-  ],
-)
+  // Sign In Route
+  AutoRoute(path: 'signin/', page: SignInPage),
+])
 class $AppRouter {}
